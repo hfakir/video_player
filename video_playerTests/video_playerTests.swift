7f004c937f442190806d71e96d2435a9d95ff78f
Hicham <hichamfakir300@gmail.com>
2026-05-20 16:24:04 +0100

    Initial Commit

---
+++ video_playerTests/video_playerTests.swift
@@ -0,0 +1,17 @@
+//
+//  video_playerTests.swift
+//  video_playerTests
+//
+//  Created by hfakir on 5/20/26.
+//
+
+import Testing
+@testable import video_player
+
+struct video_playerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
